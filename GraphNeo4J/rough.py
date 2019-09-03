e4cb101e31bcac3fe20220119b9522c96d53ecb1
Luis Bobadilla <happystep@ksu.edu>
2019-09-03 14:49:04 -0500

    changes to rough to point at cleaned datasert

--- GraphNeo4J/rough.py
+++ GraphNeo4J/rough.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-url = "http://people.cs.ksu.edu/~happystep/HPC/test_hpc"
+url = "http://people.cs.ksu.edu/~happystep/HPC/slurmCleaned"
 
 data = pd.read_csv(url)
 
@@ -8,10 +8,15 @@ l = []
 for i in data.columns.unique():
     l.append(i)
 
-string_set = []
-
+# we are going to instead print every item in l on its own line to read better
 for i in l:
-    string_set.append("CREATE INDEX ON :USER ({0});".format(i))
-
-for i in string_set:
     print(i)
+
+#
+# string_set = []
+#
+# for i in l:
+#     string_set.append("CREATE INDEX ON :USER ({0});".format(i))
+#
+# for i in string_set:
+#     print(i)
