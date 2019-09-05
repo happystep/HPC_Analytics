26c4b4d47426abc1b4b595327930109f6ff31305
Luis Bobadilla <superluis5@gmail.com>
2019-09-05 17:31:30 -0500

    had a file duplicated

--- aggregation.py
+++ aggregation.py
@@ -1,26 +1,27 @@
+# author jared marolf
 import pandas as pd
 
 df = pd.read_csv('16mInfo+reqTMcuma.csv')
 
 df.shape
 print(df.shape)
-print(df.dtypes)
 
-temp = df[['owner', 'failed', 'project', 'cpu', 'maxvmem', 'reqTime', 'reqMem', 'people','submission_time',
-    'job_number']]
+temp = df[['owner', 'failed', 'project', 'cpu', 'maxvmem', 'reqTime', 'reqMem', 'people','submission_time', 'job_number']]
 minM = temp.groupby('owner', as_index=False)['cpu', 'reqMem'].transform('min')
 minM.columns = ['minCPU', 'minReqMem']
 
 maxM = temp.groupby('owner', as_index=False)['cpu', 'reqMem'].transform('max')
 maxM.columns = ['maxCPU', 'maxReqMem']
 
-std = temp.groupby('owner', as_index=False)['cpu', 'reqMem'].std()
-cnt = temp.groupby('owner', as_index=False)['job_number'].count(axis='columns')
+std = temp.groupby('owner')['cpu', 'reqMem'].transform('std')
+std.columns = ['stdCPU', 'stdReqMem']
+cnt = temp.groupby('owner', as_index=False)['job_number'].transform('count')
+cnt.columns = ['cntUser']
 
-df = df.join(std)
-df = df.join(cnt)
 df = df.join(minM)
 df = df.join(maxM)
-print(df.shape)
-df.to_csv('16mInfo+reqTMcumamaxminstdcount.csv')
+df = df.join(std)
+df = df.join(cnt)
 
+print(df)
+df.to_csv('16mInfo+reqTMcumamaxminstdcnt.csv')
