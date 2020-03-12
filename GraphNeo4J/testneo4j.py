d7fd960c92b6e6868064ba2e64d4e2dd4f4fc7e2
Luis Bobadilla <superluis5@gmail.com>
2020-03-12 09:48:46 -0500

    added time of connection param

--- GraphNeo4J/testneo4j.py
+++ GraphNeo4J/testneo4j.py
@@ -4,7 +4,7 @@ from neo4j.v1 import GraphDatabase
 class HPCJobDatabase(object):
 
     def __init__(self, uri, user, password):
-        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+        self._driver = GraphDatabase.driver(uri, auth=(user, password), max_connection_lifetime=1080)
 
     def close(self):
         self._driver.close()
