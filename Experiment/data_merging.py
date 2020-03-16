9e9bb4bae7b91682bb619da456d264c8bbce3e0e
Luis Bobadilla <happystep@ksu.edu>
2020-03-15 22:29:57 -0500

    started file for combining data

---
+++ Experiment/data_merging.py
@@ -0,0 +1,10 @@
+# this file will merge node assignments with data from the nodes and decrease the feature size
+
+import pandas as pd
+
+
+node_role_assignment_url = "http://people.cs.ksu.edu/~happystep/HPC/node_role_assignment.csv"
+features_extracted_url = "http://people.cs.ksu.edu/~happystep/HPC/features_extracted.csv"
+full_set_url = "http://people.cs.ksu.edu/~happystep/HPC/full_set.csv"
+node_role_membership_by_percentage_url = "http://people.cs.ksu.edu/~happystep/HPC/node_role_membership_by_percentage.csv"
+
