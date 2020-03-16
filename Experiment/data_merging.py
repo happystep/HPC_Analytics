1fd68450e05cdcd06a7d0aef5ad26b62247fe73a
Luis Bobadilla <happystep@ksu.edu>
2020-03-16 09:04:46 -0500

    data merging complete

--- Experiment/data_merging.py
+++ Experiment/data_merging.py
@@ -2,9 +2,31 @@
 
 import pandas as pd
 
-
+# URL Links to be kept when code published
+full_set_url = "http://people.cs.ksu.edu/~happystep/HPC/full_set.csv"  #not used
+reduced_set_url = "http://people.cs.ksu.edu/~happystep/HPC/reduced_set.csv"
 node_role_assignment_url = "http://people.cs.ksu.edu/~happystep/HPC/node_role_assignment.csv"
 features_extracted_url = "http://people.cs.ksu.edu/~happystep/HPC/features_extracted.csv"
-full_set_url = "http://people.cs.ksu.edu/~happystep/HPC/full_set.csv"
 node_role_membership_by_percentage_url = "http://people.cs.ksu.edu/~happystep/HPC/node_role_membership_by_percentage.csv"
+transposed_roles_url = "http://people.cs.ksu.edu/~happystep/HPC/transposed_roles.csv"
+
+
+# LOCAL FILES TO BE DELETED, not in repo
+node_role_assignment_local = "../Data/node_role_assignment.csv"
+reduced_set_local = "../Data/reduced_set.csv"
+transposed_roles_local = "../Data/transposed_roles.csv"
+
+# transposed_roles = pd.read_csv(transposed_roles_url)
+transposed_roles = pd.read_csv(transposed_roles_local)  # change to URL above
+print(transposed_roles)
+
+# reduced_set = pd.read_csv(reduced_set_url)
+reduced_set = pd.read_csv(reduced_set_local)  # CHANGE TO URL above
+print(reduced_set)
+
+
+merged_set = reduced_set.set_index('ID(n)').join(transposed_roles.set_index('node'))
+print(merged_set)
+
+merged_set.to_csv("../Data/merged_set.csv")
 
